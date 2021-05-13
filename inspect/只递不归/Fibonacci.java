21388c0bb1a05784a6e444778a08e7d41c160e91
zxc <zhouxucheng_edu@163.com>
2021-05-13 11:23:20 +0800

    1st homework

---
+++ inspect/只递不归/Fibonacci.java
@@ -0,0 +1,50 @@
+// 组会思考题：只递不归的斐波那契数列
+import java.util.Scanner;
+
+public class Fibonacci{
+
+	public static void main(String[] args) {
+
+		Test t1 = new Test();
+		// t1.printFibonacci();
+
+		System.out.print("请输入斐波那契数列的个数：");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] fi = new int[n];
+		int a = t1.fibonacci(n,fi);
+		
+		for(int i = 0;i < fi.length;i++){
+			System.out.print(fi[i] + " ");
+		}
+
+	}
+}
+
+class Test{
+
+	public int fibonacci(int n,int[] fi){
+		int i = n-1;
+		if(n==1 || n==2){
+			fi[i] = 1;
+			return fi[i];
+		}else{
+			fi[i] = fibonacci(n-1,fi) + fibonacci(n-2,fi);
+			return fi[i];		
+		}
+	}
+
+
+	// public void printFibonacci(){
+	// 	System.out.print("请输入斐波那契数列的个数：");
+	// 	Scanner sc = new Scanner(System.in);
+	// 	int n = sc.nextInt();
+	// 	int[] fi = new int[n];
+	// 	int a = fibonacci(n,fi);
+	// 	for(int i = 0;i < fi.length;i++){
+	// 		System.out.print(fi[i] + " ");
+	// 	}
+	// }
+
+
+}
